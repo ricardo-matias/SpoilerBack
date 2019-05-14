2ea8b6c48df0d056937eb12c6076f3d5d4079728
sandieg2k <sandieg2k@gmail.com>
2019-05-13 22:01:39 -0300

    ajustes ao popular movies

--- src/repositories/movie-repository.js
+++ src/repositories/movie-repository.js
@@ -6,16 +6,16 @@ exports.getAll = async() => {
 }
 
 exports.create = async(request_data) => {
-    let Movie = new Movie(request_data);
-    await Movie.save();
+    let movie = new Movie(request_data);
+    await movie.save();
 }
 
 exports.delete = async(request_id) => {
-    await Movie.findByIdAndDelete({_id: request_id})
+    await movie.findByIdAndDelete({_id: request_id})
     //console.log(User.findByIdAndDelete({_id: request_id}))
 }
 
 exports.deleteAll = async() => {
-    await Movie.remove({})
+    await movie.remove({})
     //console.log(User.findByIdAndDelete({_id: request_id}))
 }
